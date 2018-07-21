8e185edd9c65f622b4ae49becf1e1ba3e7964a9e
pn69nq <baichi10086@sina.com>
2018-07-21 16:56:35 +0800

    init project

---
+++ src/test/java/com/pq/api/user/controller/ControllerUserTest.java
@@ -0,0 +1,24 @@
+package com.pq.api.user.controller;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * <pre>
+ *     author : chenzengqiang
+ *     e-mail : chenzengqiang@hongdoujiao.com
+ *     time   : 2018/7/21
+ *     desc   :
+ *     version: 1.0
+ * </pre>
+ */
+public class ControllerUserTest {
+
+    @Test
+    public void login() {
+
+
+
+    }
+}
